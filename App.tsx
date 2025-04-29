9d5c15d20e937c6f13493ba435f7cd7afbcc81c1
Remerson Monteiro <kairemerson@gmail.com>
2025-04-29 10:33:53 -0300

    config styled components, theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 
+import { ThemeProvider } from 'styled-components/native';
+import {useFonts, Nunito_400Regular, Nunito_700Bold} from "@expo-google-fonts/nunito"
+
 import { Home } from './src/screens/Home';
+import theme from './src/theme';
+import { Loading } from '@components/Loading';
+import { StatusBar } from 'react-native';
+import { Statistic } from '@screens/Statistic';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Nunito_400Regular, Nunito_700Bold})
+
+
   return (
-    <Home/>
+    <ThemeProvider theme={theme}>
+
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Statistic/> : <Loading/>}
+
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+    COLORS: {
+      'red-dark': '#bf3b44',
+      'red-mid': '#F3BABD',
+      'red-light': '#F4E6E7',
+  
+      'green-dark': '#639339',
+      'green-mid': '#CBE4B4',
+      'green-light': '#E5F0DB',
+  
+      'gray-1': '#1b1d1e',
+      'gray-2': '#333638',
+      'gray-3': '#5C6265',
+      'gray-4': '#B9BBBC',
+      'gray-5': '#DDDEDF',
+      'gray-6': '#EFF0F0',
+      'gray-7': '#FAFAFA',
+  
+      white: '#FFF'
+    },
+    FONT_SIZE: {
+      SM: 12,
+      MD: 14,
+      LG: 16,
+      XL: 18,
+      '2XL': 24,
+      '3XL': 32
+    },
+    FONT_FAMILY: {
+      REGULAR: 'Nunito_400Regular',
+      BOLD: 'Nunito_700Bold'
+    }
+  } as const
